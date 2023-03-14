4c404a08de857ddc58bd5c9450808c4a280d23dd
danielbl <danielbl@DAW01>
2023-03-14 11:17:20 +0100

    Cambié el nombre de Jaime a J

--- Git/src/git/Git.java
+++ Git/src/git/Git.java
@@ -8,7 +8,7 @@ public class Git {
         
         int numero=0;
         
-        String nombre="JAIME";
+        String nombre="J";
         
         System.out.println("Modificacion");
         
